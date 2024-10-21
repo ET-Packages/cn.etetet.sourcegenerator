f2511f9b3268b2ba36565be6906460a3d6bef785
RabbitCocoa <57069237+RabbitCocoa@users.noreply.github.com>
2024-10-21 21:50:50 +0800

    修改Async分析器只作用于ET相关的程序集 (#1)

--- DotNet~/ET.SourceGenerator/Analyzer/AsyncMethodReturnTypeAnalyzer.cs
+++ DotNet~/ET.SourceGenerator/Analyzer/AsyncMethodReturnTypeAnalyzer.cs
@@ -26,6 +26,10 @@ namespace ET
 
         private void Analyzer(SyntaxNodeAnalysisContext context)
         {
+            if (!AnalyzerHelper.IsAssemblyNeedAnalyze(context.Compilation.AssemblyName, AnalyzeAssembly.All))
+            {
+                return;
+            }
             IMethodSymbol? methodSymbol = null;
             
             if (context.Node is MethodDeclarationSyntax methodDeclarationSyntax)
